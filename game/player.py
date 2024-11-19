0cb3c2755c6a4c34c91e4a5eec8f81ce120cac85
Vu <vukhanh732@DESKTOP-HVJUMOE>
2024-11-20 02:46:19 +1100

    Add bet, raise, fold methods

--- game/player.py
+++ game/player.py
@@ -6,11 +6,19 @@ class Player:
         self.chips = chips
         self.hand = []
         self.active = True
+        self.current_bet = 0  # Track player's bet in the current round
 
     def receive_cards(self, cards):
         self.hand.extend(cards)
 
     def bet(self, amount):
-        actual_bet = min(self.chips, amount)
-        self.chips -= actual_bet
-        return actual_bet
+        bet_amount = min(self.chips, amount)
+        self.chips -= bet_amount
+        self.current_bet += bet_amount
+        return bet_amount
+
+    def reset_bet(self):
+        self.current_bet = 0
+
+    def fold(self):
+        self.active = False
